476eb8c0a629856a81616ba91e98d96730c12035
gyoridanidavid <gyiri24@gmail.com>
2023-04-22 10:20:08 +0200

    make auth controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,4 +66,16 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    public function store(UserRequest $request)
+    {
+        $data = $request->only(['name', 'email', 'password']);
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+
+        return response()->json(
+            UserResource::make($user),
+            Response::HTTP_CREATED
+        );
+    }
 }
